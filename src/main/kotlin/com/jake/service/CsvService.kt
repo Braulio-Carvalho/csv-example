6d4da27ce57b447bfb0902a24823fdc9a0a665b0
Braulio Carvalho <braulio.github@gmail.com>
2021-10-05 21:09:52 -0300

    commit initial

--- src/main/kotlin/com/jake/service/CsvService.kt
+++ src/main/kotlin/com/jake/service/CsvService.kt
@@ -9,17 +9,13 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 @Singleton
-class CsvService() {
+class CsvService {
 
     fun generateFileCSV(nameFile: String, client: Client) {
-        val newCsvPath = CSV_File_Path(nameFile)
-        val writer = Files.newBufferedWriter(Paths.get(newCsvPath))
-        val csvPrinter = CSVPrinter(
-            writer, CSVFormat.DEFAULT
-        )
+        val writer = Files.newBufferedWriter(Paths.get(CSV_File_Path(nameFile)))
+        val csvPrinter = CSVPrinter(writer, CSVFormat.DEFAULT)
         csvPrinter.printRecord(client.cpf, client.firstName, client.lastName, client.transactionAmount.toString())
         csvPrinter.flush()
         csvPrinter.close()
     }
 }
-
